557831bec3ec3d6938b22a5645cbc105b39d9d39
mwu515 <mian.wu515@duke.edu>
2023-02-08 21:43:45 +0000

    assertion in wk2

--- week2/src/lib.rs
+++ week2/src/lib.rs
@@ -4,7 +4,8 @@ pub fn run_command(absolute_path_to_forked_project: String, upstream_addr: Strin
     // upstreamAddr example: git@github.com:mianwu515/rust-world-spr23.git
     // branchName: main/master
     assert!(run_cmd!(cd "$absolute_path_to_forked_project").is_ok());
-    run_cmd!(git remote add upstream "$upstream_addr"); // remote upstream may have already existed. No assertion here
+    assert!(run_cmd!(git remote remove upstream).is_ok()); 
+    assert!(run_cmd!(git remote add upstream "$upstream_addr").is_ok());
     assert!(run_cmd!{
         git fetch upstream;
         git checkout "$branch_name";
