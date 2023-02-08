ebe79d1130eab95867591a4ba1fd2b3585121d7d
mwu515 <mian.wu515@duke.edu>
2023-02-08 21:40:50 +0000

    wk2 cli modify

--- week2/src/main.rs
+++ week2/src/main.rs
@@ -14,11 +14,6 @@ struct Cli {
 
 #[derive(Parser)]
 enum Commands {
-    #[clap(
-        version = "1.0",
-        author = "Mian Wu",
-        about = "A tool to sync the forked origin with the upstream."
-    )]
     Syncfork {
         #[clap(short, long)]
         path: String,
@@ -33,7 +28,7 @@ fn main() {
     println!("Usage:");
     println!("cargo run -- syncfork --path [your-local-project-path] --upstream [upstream-url] --branch [your-remote-repo-branch]");
     println!("For example:");
-    println!("cargo run -- syncfork --path ~/rust-world-spr23 --upstream git@github.com:mianwu515/rust-world-spr23.git --branch main");
+    println!("cargo run -- syncfork --path ~/rust-world-spr23 --upstream git@github.com:mianwu515/rust-world-spr23.git --branchmain");
     let args = Cli::parse();
     match args.command {
         Some(Commands::Syncfork { path, upstream, branch }) => run_command(path, upstream, branch),
