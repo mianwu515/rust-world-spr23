86993fce9e2b5fd5f1339f59648948419a4bcd62
mwu515 <mian.wu515@duke.edu>
2023-02-08 21:46:57 +0000

    git cli debugging

--- week2/src/lib.rs
+++ week2/src/lib.rs
@@ -6,11 +6,10 @@ pub fn run_command(absolute_path_to_forked_project: String, upstream_addr: Strin
     assert!(run_cmd!(cd "$absolute_path_to_forked_project").is_ok());
     assert!(run_cmd!(git remote remove upstream).is_ok()); 
     assert!(run_cmd!(git remote add upstream "$upstream_addr").is_ok());
-    assert!(run_cmd!{
-        git fetch upstream;
-        git checkout "$branch_name";
-        git pull upstream "$branch_name";
-        git push origin "$branch_name";
-    }.is_ok());
+    assert!(run_cmd!(git fetch upstream).is_ok());
+    assert!(run_cmd!(git checkout "$branch_name";).is_ok());
+    assert!(run_cmd!(git pull upstream "$branch_name").is_ok());
+    assert!(run_cmd!(git push origin "$branch_name").is_ok());
+   // assert!(run_cmd!(git merge upstream/"$branch_name";).is_ok());
 }
 
