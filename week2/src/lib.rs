9264c91b5ec80b5318f7b9d2ddb6662cb68b5e11
mw515 <mw515@duke.edu>
2023-02-08 17:33:44 -0500

    debug

--- week2/src/lib.rs
+++ week2/src/lib.rs
@@ -3,14 +3,13 @@ use cmd_lib::run_cmd;
 pub fn run_command(absolute_path_to_forked_project: String, upstream_addr: String, branch_name: String) {
     // upstreamAddr example: git@github.com:mianwu515/rust-world-spr23.git
     // branchName: main/master
-    assert!(run_cmd!(cd "$absolute_path_to_forked_project").is_ok());
-    assert!(run_cmd!(git remote remove upstream).is_ok()); 
-    assert!(run_cmd!(git remote add upstream "$upstream_addr").is_ok());
-    assert!(run_cmd!(git fetch upstream).is_ok());
-    assert!(run_cmd!(git merge upstream/"$branch_name";).is_ok());
-    //assert!(run_cmd!(git checkout "$branch_name";).is_ok());
-    //assert!(run_cmd!(git pull upstream "$branch_name").is_ok());
-    assert!(run_cmd!(git push origin "$branch_name").is_ok());
-   // assert!(run_cmd!(git merge upstream/"$branch_name";).is_ok());
+   assert!(run_cmd!(cd "/Users/wumian/Documents/Spring2023Courses/ECE651SoftwareEngineering/gitlab/ece651-spr23").is_ok());
+   run_cmd!(git remote add upstream "git@github.com:mianwu515/rust-world-spr23.git");
+   assert!(run_cmd!{
+        git fetch upstream;
+        git checkout master;
+        git pull upstream master;
+        git push origin master;
+    }.is_ok());
 }
 
