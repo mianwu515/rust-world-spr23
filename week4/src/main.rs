e0fab7dd6e70a7447d6524adc72c193935706a0d
mianwu-cloud9 <mian.wu515@duke.edu>
2023-02-13 19:40:40 +0000

    wk4

--- week4/src/main.rs
+++ week4/src/main.rs
@@ -1,43 +1,30 @@
-// An example of generating julia fractals.
+extern crate lyon_geom;
+extern crate lyon_svg;
+
+use lyon_geom::math::Point;
+use lyon_geom::path::{builder, Path, PathEvent};
+use lyon_geom::svg::PathWriter;
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    let imgx = 800;
-    let imgy = 800;
+    let mut path_builder = builder::PathBuilder::new();
 
-    let scalex = 3.0 / imgx as f32;
-    let scaley = 3.0 / imgy as f32;
-
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
-
-    // Iterate over the coordinates and pixels of the image
-    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (0.5 * x as f32) as u8;
-        let g = (0.1 * (x + y) as f32) as u8;
-        let b = (0.4 * y as f32) as u8;
-        *pixel = image::Rgb([r, g, b]);
+    for i in 0..360 {
+        let angle = i as f32 * std::f32::consts::PI / 180.0;
+        let radius = (angle.sin() * angle.sin() * angle.cos()).abs() * 100.0;
+        let x = radius * angle.cos() + 200.0;
+        let y = radius * angle.sin() + 200.0;
+        path_builder.path_event(PathEvent::LineTo(Point { x, y }));
     }
 
-    // A redundant loop to demonstrate reading image data
-    for x in 0..imgx {
-        for y in 0..imgy {
-            let cx = y as f32 * scalex - 1.5;
-            let cy = x as f32 * scaley - 1.5;
+    let path = path_builder.build();
 
-            let c = num_complex::Complex::new(-0.4, 0.6);
-            let mut z = num_complex::Complex::new(cx, cy);
-
-            let mut i = 0;
-            while i < 255 && z.norm() <= 2.0 {
-                z = z * z * z + c;
-                i += 1;
-            }
-
-            let pixel = imgbuf.get_pixel_mut(x, y);
-            let image::Rgb(data) = *pixel;
-            *pixel = image::Rgb([data[0], i as u8, data[2]]);
-        }
+    let mut file = File::create("rose.svg").unwrap();
+    let mut buffer = Vec::new();
+    {
+        let mut writer = PathWriter::new(&mut buffer);
+        writer.write_path(&path, &lyon_svg::Options::default()).unwrap();
     }
-
-    // Save the image as “fractal.png”, the format is deduced from the path
-    imgbuf.save("fractal.png").unwrap();
+    file.write_all(&buffer).unwrap();
 }
