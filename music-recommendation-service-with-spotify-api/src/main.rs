9ec52a7782bea8392647200b9336d48f432f6ff9
mwu515 <mian.wu515@duke.edu>
2023-02-20 23:34:09 +0000

    music

--- music-recommendation-service-with-spotify-api/src/main.rs
+++ music-recommendation-service-with-spotify-api/src/main.rs
@@ -1,7 +1,13 @@
 // ! A Rust Actix microserver music recommendation service with Spotify API 
 // ! by providing a genre.
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+/*An actix Microservice that has multiple routes:
+A.  / that turns a hello world
+B. /possible-genres that returns a list of possible genres
+C. /genre/{genre} that returns a list of recommendations for the genre
+*/
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
@@ -10,6 +16,31 @@ struct AccessTokenResponse {
     access_token: String,
     token_type: String,
     expires_in: u32,
+}
+
+// A.  / that turns a hello world
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+// B. /possible-genres that returns a list of possible genres
+#[get("/possible-genres")]
+async fn possible_genres() -> impl Responder {
+    let access_token = get_access_token("369dbd7452cf4e5fac093191ef5e6538",
+    "9eee9c5a46834c0b8319c5e0c798b4f4").unwrap();
+    let genres = get_possible_genres(&access_token).unwrap();
+    HttpResponse::Ok().body(format!("{:?}", genres));
+}
+
+// C. /{genre} that returns a list of recommendations for the genre
+#[get("/genre/{genre}")]
+async fn genre_recommendations(genre: web::Path<String>) -> impl Responder {
+    let access_token = get_access_token("369dbd7452cf4e5fac093191ef5e6538",
+    "9eee9c5a46834c0b8319c5e0c798b4f4").unwrap();
+
+    let recommendations = get_recommendations(&access_token, &genre).unwrap();
+    HttpResponse::Ok().body(format!("{:?}", recommendations));
 }
 
 fn get_access_token(client_id: &str, client_secret: &str) ->   Result<String, Box<dyn std::error::Error>> {
@@ -56,8 +87,32 @@ fn get_recommendations(access_token: &str, genre: &str) -> Result<Vec<Track>, Bo
     Ok(response.tracks)
 }
 
+fn get_possible_genres(access_token: &str) -> Result<RecommendationResponse, Box<dyn std::error::Error>>{
+    let client = reqwest::blocking::Client::new();
+    let mut headers = reqwest::header::HeaderMap::new();
+    headers.insert(reqwest::header::AUTHORIZATION, format!("Bearer {}", access_token).parse().unwrap());
 
+    let response = client.get("	https://api.spotify.com/v1/recommendations/available-genre-seeds")
+        .headers(headers)
+        .send()?
+        .json::<RecommendationResponse>()?;
+    Ok(response)
+}
 
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(possible_genres)
+            .service(genre_recommendations)
+    })
+    .bind("http://localhost:8888")? //0.0.0.0:8080
+    .run()
+    .await
+}
+
+/* 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let client_id = "369dbd7452cf4e5fac093191ef5e6538";
     let client_secret = "9eee9c5a46834c0b8319c5e0c798b4f4";
@@ -70,6 +125,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for track in recommendations {
         println!("{} by {}", track.name, track.artists[0].name);
     }
-
     Ok(())
 }
+*/
