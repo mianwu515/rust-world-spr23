7fca4ad33a39e339cb0f1b3b313bbbb4dacc3db8
mwu515 <mian.wu515@duke.edu>
2023-03-29 23:17:00 +0000

    chatbot

--- week12/src/main.rs
+++ week12/src/main.rs
@@ -1,134 +1,253 @@
-mod config {
-    use serde::Deserialize;
-    #[derive(Debug, Default, Deserialize)]
-    pub struct ExampleConfig {
-        pub server_addr: String,
-        pub pg: deadpool_postgres::Config,
-    }
+// This program generates a random Sudoku puzzle, then allows the user to solve it by entering numbers into the grid. The user can move the cursor around the grid using the arrow keys, and can delete a number by pressing backspace. When the user has entered a valid solution, the program congratulates them and exits.
+
+use crossterm::event::{Event, KeyCode, KeyEvent};
+use crossterm::{cursor, event, execute, queue, style, terminal};
+use crossterm::cursor::MoveTo;
+use rand::Rng;
+use std::fmt;
+use std::io::{self, Write};
+
+const BOARD_SIZE: usize = 9;
+
+struct Board {
+    board: [[u8; BOARD_SIZE]; BOARD_SIZE],
 }
 
-mod models {
-    use serde::{Deserialize, Serialize};
-    use tokio_pg_mapper_derive::PostgresMapper;
-
-    #[derive(Deserialize, PostgresMapper, Serialize)]
-    #[pg_mapper(table = "users")] // singular 'user' is a keyword..
-    pub struct User {
-        pub email: String,
-        pub first_name: String,
-        pub last_name: String,
-        pub username: String,
+impl Board {
+    fn new() -> Self {
+        Self {
+            board: [[0; BOARD_SIZE]; BOARD_SIZE],
+        }
     }
-}
 
-mod errors {
-    use actix_web::{HttpResponse, ResponseError};
-    use deadpool_postgres::PoolError;
-    use derive_more::{Display, From};
-    use tokio_pg_mapper::Error as PGMError;
-    use tokio_postgres::error::Error as PGError;
-
-    #[derive(Display, From, Debug)]
-    pub enum MyError {
-        NotFound,
-        PGError(PGError),
-        PGMError(PGMError),
-        PoolError(PoolError),
+    fn print(&self, cursor_pos: (u16, u16)) {
+        for (i, row) in self.board.iter().enumerate() {
+            for (j, &cell) in row.iter().enumerate() {
+                let ch = if cell != 0 {
+                    format!("{}", cell)
+                } else {
+                    " ".to_owned()
+                };
+                let color = if self.is_conflict(i, j) {
+                    style::Color::Red
+                } else {
+                    style::Color::White
+                };
+                execute!(
+                    io::stdout(),
+                    MoveTo(cursor_pos.0 + j as u16 * 2, cursor_pos.1 + i as u16),
+                    style::PrintStyledContent(style::StyledContent::new(ch).with(color)),
+                )
+                .unwrap();
+            }
+        }
+        io::stdout().flush().unwrap();
     }
-    impl std::error::Error for MyError {}
 
-    impl ResponseError for MyError {
-        fn error_response(&self) -> HttpResponse {
-            match *self {
-                MyError::NotFound => HttpResponse::NotFound().finish(),
-                MyError::PoolError(ref err) => {
-                    HttpResponse::InternalServerError().body(err.to_string())
+    fn is_valid(&self, row: usize, col: usize, num: u8) -> bool {
+        for i in 0..BOARD_SIZE {
+            if self.board[row][i] == num {
+                return false;
+            }
+            if self.board[i][col] == num {
+                return false;
+            }
+            let box_row = row / 3 * 3 + i / 3;
+            let box_col = col / 3 * 3 + i % 3;
+            if self.board[box_row][box_col] == num {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn is_conflict(&self, row: usize, col: usize) -> bool {
+        let num = self.board[row][col];
+        if num == 0 {
+            return false;
+        }
+        for i in 0..BOARD_SIZE {
+            if i != col && self.board[row][i] == num {
+                return true;
+            }
+            if i != row && self.board[i][col] == num {
+                return true;
+            }
+            let box_row = row / 3 * 3 + i / 3;
+            let box_col = col / 3 * 3 + i % 3;
+            if box_row != row && box_col != col && self.board[box_row][box_col] == num {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn is_solved(&self) -> bool {
+        for i in 0..BOARD_SIZE {
+            for j in 0..BOARD_SIZE {
+                if self.board[i][j] == 0 || self.is_conflict(i, j) {
+                    return false;
                 }
-                _ => HttpResponse::InternalServerError().finish(),
+            }
+        }
+        true
+    }
+
+    fn set_cell(&mut self, row: usize, col: usize, num: u8) {
+        self.board[row][col] = num;
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for (i, row) in self.board.iter().enumerate() {
+            if i % 3 == 0 {
+                writeln!(f, "+-------+-------+-------+").unwrap();
+            }
+            for (j, &cell) in row.iter().enumerate() {
+                if j % 3 == 0 {
+                    write!(f, "| ")?;
+                }
+                if cell != 0 {
+                    write!(f, "{} ", cell)?;
+                } else {
+                    write!(f, "  ")?;
+                }
+            }
+            writeln!(f, "|")?;
+        }
+        writeln!(f, "+-------+-------+-------+")
+    }
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut board = Board::new();
+    // Generate a random puzzle
+    for i in 0..BOARD_SIZE {
+        let mut nums: Vec<u8> = (1..=9).collect();
+        rng.shuffle(&mut nums);
+        for j in 0..BOARD_SIZE {
+            let num = nums.pop().unwrap();
+            if !board.is_valid(i, j, num) {
+                nums.insert(0, num);
+                continue;
+            }
+            board.set_cell(i, j, num);
+        }
+    }
+
+    let mut cursor_pos = (0, 0);
+    let mut selected_cell = (0, 0);
+
+    execute!(
+        io::stdout(),
+        terminal::Clear(terminal::ClearType::All),
+        cursor::Hide,
+    )
+    .unwrap();
+
+    loop {
+        board.print(cursor_pos);
+        queue!(
+            io::stdout(),
+            MoveTo(
+                cursor_pos.0 + selected_cell.1 as u16 * 2,
+                cursor_pos.1 + selected_cell.0 as u16
+            ),
+            style::SetForegroundColor(style::Color::Black),
+            style::SetBackgroundColor(style::Color::White),
+            style::Print("  "),
+            style::ResetColor,
+        )
+        .unwrap();
+        io::stdout().flush().unwrap();
+
+        if let Ok(Event::Key(KeyEvent { code, .. })) = event::read() {
+            match code {
+                KeyCode::Char('1')
+                | KeyCode::Char('2')
+                | KeyCode::Char('3')
+                | KeyCode::Char('4')
+                | KeyCode::Char('5')
+                | KeyCode::Char('6')
+                | KeyCode::Char('7')
+                | KeyCode::Char('8')
+                | KeyCode::Char('9') => {
+                    let num = code as u8 - b'0';
+                    if board.is_valid(selected_cell.0, selected_cell.1, num) {
+                        board.set_cell(selected_cell.0, selected_cell.1, num);
+                        selected_cell.1 += 1;
+                        if selected_cell.1 == BOARD_SIZE {
+                            selected_cell.0 += 1;
+                            selected_cell.1 = 0;
+                            if selected_cell.0 == BOARD_SIZE {
+                                selected_cell.0 = 0;
+                            }
+                        }
+                    }
+                }
+                KeyCode::Backspace => {
+                    board.set_cell(selected_cell.0, selected_cell.1, 0);
+                    if selected_cell.1 > 0 {
+                        selected_cell.1 -= 1;
+                    } else {
+                        selected_cell.0 -= 1;
+                        if selected_cell.0 == usize::max_value() {
+                            selected_cell.0 = BOARD_SIZE - 1;
+                        }
+                        selected_cell.1 = BOARD_SIZE - 1;
+                    }
+                }
+                KeyCode::Enter => {
+                    if board.is_solved() {
+                        execute!(
+                            io::stdout(),
+                            cursor::Show,
+                            terminal::Clear(terminal::ClearType::All),
+                            MoveTo(0, 0),
+                            style::Print("Congratulations, you solved the puzzle!"),
+                        )
+                        .unwrap();
+                        break;
+                    } else {
+                        execute!(
+                            io::stdout(),
+                            cursor::Hide,
+                            MoveTo(0, 0),
+                            style::Print("The puzzle is not solved yet. Keep trying!"),
+                        )
+                        .unwrap();
+                    }
+                }
+                KeyCode::Up => {
+                    if selected_cell.0 > 0 {
+                        selected_cell.0 -= 1;
+                    } else {
+                        selected_cell.0 = BOARD_SIZE - 1;
+                    }
+                }
+                KeyCode::Down => {
+                    selected_cell.0 += 1;
+                    if selected_cell.0 == BOARD_SIZE {
+                        selected_cell.0 = 0;
+                    }
+                }
+                KeyCode::Left => {
+                    if selected_cell.1 > 0 {
+                        selected_cell.1 -= 1;
+                    } else {
+                        selected_cell.1 = BOARD_SIZE - 1;
+                    }
+                }
+                KeyCode::Right => {
+                    selected_cell.1 += 1;
+                    if selected_cell.1 == BOARD_SIZE {
+                        selected_cell.1 = 0;
+                    }
+                }
+                _ => {}
             }
         }
     }
-}
-
-mod db {
-    use deadpool_postgres::Client;
-    use tokio_pg_mapper::FromTokioPostgresRow;
-
-    use crate::{errors::MyError, models::User};
-
-    pub async fn add_user(client: &Client, user_info: User) -> Result<User, MyError> {
-        let _stmt = include_str!("../sql/add_user.sql");
-        let _stmt = _stmt.replace("$table_fields", &User::sql_table_fields());
-        let stmt = client.prepare(&_stmt).await.unwrap();
-
-        client
-            .query(
-                &stmt,
-                &[
-                    &user_info.email,
-                    &user_info.first_name,
-                    &user_info.last_name,
-                    &user_info.username,
-                ],
-            )
-            .await?
-            .iter()
-            .map(|row| User::from_row_ref(row).unwrap())
-            .collect::<Vec<User>>()
-            .pop()
-            .ok_or(MyError::NotFound) // more applicable for SELECTs
-    }
-}
-
-mod handlers {
-    use actix_web::{web, Error, HttpResponse};
-    use deadpool_postgres::{Client, Pool};
-
-    use crate::{db, errors::MyError, models::User};
-
-    pub async fn add_user(
-        user: web::Json<User>,
-        db_pool: web::Data<Pool>,
-    ) -> Result<HttpResponse, Error> {
-        let user_info: User = user.into_inner();
-
-        let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
-
-        let new_user = db::add_user(&client, user_info).await?;
-
-        Ok(HttpResponse::Ok().json(new_user))
-    }
-}
-
-use ::config::Config;
-use actix_web::{web, App, HttpServer};
-use dotenv::dotenv;
-use handlers::add_user;
-use tokio_postgres::NoTls;
-
-use crate::config::ExampleConfig;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    dotenv().ok();
-
-    let config_ = Config::builder()
-        .add_source(::config::Environment::default())
-        .build()
-        .unwrap();
-
-    let config: ExampleConfig = config_.try_deserialize().unwrap();
-
-    let pool = config.pg.create_pool(None, NoTls).unwrap();
-
-    let server = HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(pool.clone()))
-            .service(web::resource("/users").route(web::post().to(add_user)))
-    })
-    .bind(config.server_addr.clone())?
-    .run();
-    println!("Server running at http://{}/", config.server_addr);
-
-    server.await
 }
